b3a6bff00f69d325d418bec8b15337d8531af17c
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-10-10 17:46:21 -0400

    ecommerce app

---
+++ mod6/e-commerce-system/src/models/Product.ts
@@ -0,0 +1,20 @@
+
+
+export default class Product {
+    price: number;
+    title: string;
+    id: number;
+
+    constructor(title: string, price: number, id: number) {
+        this.title = title;
+        this.price = price;
+        this.id = id;
+    }
+
+
+
+    displayProductDetails() {
+        
+    }
+}
+

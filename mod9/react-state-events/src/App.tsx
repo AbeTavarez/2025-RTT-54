3cf72bdcc6a3485cacf13302e9a50722fbf8926a
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-10-24 16:08:49 -0400

    state and evets

---
+++ mod9/react-state-events/src/App.tsx
@@ -0,0 +1,18 @@
+
+import Counter from './components/Counter';
+import './App.css'
+
+function App() {
+
+
+  return (
+    <div>
+      <h1>React State & Events</h1>
+
+      <Counter />
+    </div>
+  )
+}
+
+
+export default App;

78401944daf7c3039cd415489b7eeba908a0de88
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-11-13 13:29:11 -0500

    Blog App

---
+++ mod10/blog-app/src/pages/BlogPage.tsx
@@ -0,0 +1,9 @@
+function BlogPage() {
+  return (
+    <main>
+      <h2>Blog Page</h2>
+    </main>
+  );
+}
+
+export default BlogPage;
---
+++ mod10/blog-app/src/pages/HomePage.tsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <main>
+      <h2>Home Page</h2>
+    </main>
+  );
+}
+
+export default HomePage;

ec156cee57694a1668f82bb871f3dc153dc65609
Abraham E. Tavarez <abeincwireless@gmail.com>
2025-12-01 18:50:49 -0500

    mod10 and mod13

--- mod10/recipe-discovery-app/src/App.tsx
+++ mod10/recipe-discovery-app/src/App.tsx
@@ -3,8 +3,22 @@ import { Route, Routes } from 'react-router-dom'
 import './App.css'
 import HomePage from './pages/HomePage'
 import CategoryPage from './pages/CategoryPage'
+import { useEffect } from 'react'
 
 function App() {
+
+  // useEffect(() => {
+  //   const fetchData = async() => {
+  //     const response = await fetch('/api/user', {
+  //       headers: {
+  //         'Content-Type': 'application/json',
+  //         "x-api-key": "supersecretkey"
+  //       }
+  //     })
+  //   }
+
+  //   fetchData();
+  // })
 
 
   return (
